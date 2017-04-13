8cf1652c1b6f32c20cf4ac6c14e19457a7547a31
YuboHu <yuboh@sfu.ca>
2017-04-13 16:35:05 -0700

    "remove import not needed"

--- src/fantasyoptimizer/FantasyOptimizer.java
+++ src/fantasyoptimizer/FantasyOptimizer.java
@@ -3,7 +3,6 @@ package fantasyoptimizer;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
