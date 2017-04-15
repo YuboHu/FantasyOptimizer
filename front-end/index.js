1b2efb4f01250221f0800c8022b1023c2808f1e3
朱婧敏 <454254706@163.com>
2017-04-14 18:35:29 -0700

    lineup 1.0

--- front-end/index.js
+++ front-end/index.js
@@ -8,6 +8,7 @@ window.onload = function () {
     var i;
     for(i=0; i<10; i++) {
       $('#pp-table-rank-'+i).width(window.innerWidth*0.1);
+
       $('#pp-table-pg-'+i).append($('#player-info-tmp').render([{'name':rankData.pg[i].name}]));
       $('#pp-table-pg-'+i+' > .player-name').width(window.innerWidth*0.18-11);
       $('#pp-table-pg-'+i+' > .img-bg-pair > .player-img')[0].style.backgroundImage = 'url('+rankData.pg[i].headImg+')';
@@ -27,6 +28,65 @@ window.onload = function () {
       $('#pp-table-c-'+i).append($('#player-info-tmp').render([{'name':rankData.c[i].name}]));
       $('#pp-table-c-'+i+' > .player-name').width(window.innerWidth*0.18-11);
       $('#pp-table-c-'+i+' > .img-bg-pair > .player-img')[0].style.backgroundImage = 'url('+rankData.c[i].headImg+')';
+    }
+  });
+
+  $.ajax({
+    url: 'http://192.168.0.10:8081/FantasyServer/lineup',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function (data) {
+    var i;
+    for(i=0; i<data.lineup.length; i++) {
+      var title = document.createElement('tr');
+      title.className = 'lineupX-title';
+      if(i==0) {
+        title.className += ' first-row';
+      }
+      var td = document.createElement('td');
+      td.colSpan = '5';
+      td.appendChild(document.createTextNode('阵容 ' + (i+1) + ' 预计得分：' + data.lineup[i].total.toFixed(2)));
+      title.appendChild(td);
+      $('.lineup-table tbody')[0].appendChild(title);
+
+      var players = document.createElement('tr');
+      players.className = 'lineupX-players';
+      var pg = document.createElement('td');
+      pg.id = 'lineup-'+i+'-pg';
+      players.appendChild(pg);
+      var sg = document.createElement('td');
+      sg.id = 'lineup-'+i+'-sg';
+      players.appendChild(sg);
+      var sf = document.createElement('td');
+      sf.id = 'lineup-'+i+'-sf';
+      players.appendChild(sf);
+      var pf = document.createElement('td');
+      pf.id = 'lineup-'+i+'-pf';
+      players.appendChild(pf);
+      var c = document.createElement('td');
+      c.id = 'lineup-'+i+'-c';
+      players.appendChild(c);
+      $('.lineup-table tbody')[0].appendChild(players);
+
+      $('#lineup-'+i+'-pg').append($('#lineup-player-info-tmp').render([{'position':'PG','name':data.lineup[i].pg.name}]));
+      $('#lineup-'+i+'-pg > .player-name').width(window.innerWidth*0.20-10);
+      $('#lineup-'+i+'-pg > .img-bg-pair > .player-img')[0].style.backgroundImage = 'url('+data.lineup[i].pg.headImg+')';
+
+      $('#lineup-'+i+'-sg').append($('#lineup-player-info-tmp').render([{'position':'SG','name':data.lineup[i].sg.name}]));
+      $('#lineup-'+i+'-sg > .player-name').width(window.innerWidth*0.20-10);
+      $('#lineup-'+i+'-sg > .img-bg-pair > .player-img')[0].style.backgroundImage = 'url('+data.lineup[i].sg.headImg+')';
+
+      $('#lineup-'+i+'-sf').append($('#lineup-player-info-tmp').render([{'position':'SF','name':data.lineup[i].sf.name}]));
+      $('#lineup-'+i+'-sf > .player-name').width(window.innerWidth*0.20-10);
+      $('#lineup-'+i+'-sf > .img-bg-pair > .player-img')[0].style.backgroundImage = 'url('+data.lineup[i].sf.headImg+')';
+
+      $('#lineup-'+i+'-pf').append($('#lineup-player-info-tmp').render([{'position':'PF','name':data.lineup[i].pf.name}]));
+      $('#lineup-'+i+'-pf > .player-name').width(window.innerWidth*0.20-10);
+      $('#lineup-'+i+'-pf > .img-bg-pair > .player-img')[0].style.backgroundImage = 'url('+data.lineup[i].pf.headImg+')';
+
+      $('#lineup-'+i+'-c').append($('#lineup-player-info-tmp').render([{'position':'C','name':data.lineup[i].c.name}]));
+      $('#lineup-'+i+'-c > .player-name').width(window.innerWidth*0.20-10);
+      $('#lineup-'+i+'-c > .img-bg-pair > .player-img')[0].style.backgroundImage = 'url('+data.lineup[i].c.headImg+')';
     }
   });
 }
